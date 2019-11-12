9b5cf566fc70ef9769558e10c1052c36e3e67e85
fjq <junqi.fjq@alibaba-inc.com>
2019-11-12 14:23:13 +0800

    upload electron sample

---
+++ utils.js
@@ -0,0 +1,40 @@
+function initialization(userId) {
+    if (aliWebrtc) {
+        aliWebrtc.configRemoteAudio(userId, false);
+        aliWebrtc.configRemoteCameraTrack(userId, false, false);
+        aliWebrtc.configRemoteScreenTrack(userId, false);
+    }
+}
+
+function getSubscribeInfo(userId) {
+    var userInfo = aliWebrtc.getUserInfo(userId);
+    var subscribeInfo = [], subscribeInfoArr = [], isSubAudio = false, isSubLarge = false, isSubSmall = false, isSubCamera = false, isSubScreen = false, isSubVideo = false;
+    if (userInfo) {
+        userInfo.streamConfigs.forEach(v => {
+        if (v.subscribed) {
+            subscribeInfo.push(v.label);
+            subscribeInfoArr.push(v);
+            v.type == "audio" ? isSubAudio = true : console.warn("不是音频", JSON.stringify(v), v.type);
+            v.type == "video" ? isSubVideo = true : '';
+            v.label == "sophon_video_camera_large" ? isSubLarge = true : "";
+            v.label == "sophon_video_camera_small" ? isSubSmall = true : "";
+            v.label == "sophon_video_screen_share" ? isSubScreen = true : "";
+            if (isSubLarge || isSubSmall) {
+                isSubCamera = true;
+            }
+        }
+        });
+    }
+    return { subscribeInfo: subscribeInfo, subscribeInfoArr: subscribeInfoArr, isSubLarge: isSubLarge, isSubSmall: isSubSmall, isSubCamera: isSubCamera, isSubAudio: isSubAudio, isSubScreen: isSubScreen, isSubVideo: isSubVideo };
+}
+
+Array.prototype.getIndexByProprety = function (val, proprety) {
+    var arr = this;
+    var index = -1;
+    arr.forEach((v, i, a) => {
+        if (v[proprety] == val) {
+            index = i;
+        }
+    });
+    return index;
+}
